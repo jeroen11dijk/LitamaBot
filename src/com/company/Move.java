4e5c70ccb105bb23ae0ed4981da22056082cc4d1
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-11-17 13:12:24 +0100

    Added a move class so we can actually keep track of moves in alpha beta pruning

---
+++ src/com/company/Move.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Move {
+
+    private Card card;
+    private int x;
+    private int y;
+
+    public Move(Card card, int x, int y) {
+        this.card = card;
+        this.x = x;
+        this.y = y;
+    }
+}
